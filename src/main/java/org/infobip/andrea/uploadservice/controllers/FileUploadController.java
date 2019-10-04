54d5a79fb7413f747e6362691f1fde91509eb1c2
AndDanza <andreadanzante@gmail.com>
2019-10-04 20:02:14 +0200

    Configuration and controller
    Upload size cnfiguration set and basic controller methods implemented

--- src/main/java/org/infobip/andrea/uploadservice/controllers/FileUploadController.java
+++ src/main/java/org/infobip/andrea/uploadservice/controllers/FileUploadController.java
@@ -1,6 +1,10 @@
 package org.infobip.andrea.uploadservice.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,7 +12,27 @@ import org.springframework.web.servlet.ModelAndView;
 public class FileUploadController
 {
     @GetMapping(value = "/upload")
-    public ModelAndView getUpload()
+    public ModelAndView getFileUpload()
+    {
+        return new ModelAndView("upload");
+    }
+
+    @PostMapping(value = "/api/v1/upload")
+    public ModelAndView postFileUpload(final HttpServletRequest request, final HttpServletResponse response)
+    {
+        return new ModelAndView("upload");
+    }
+
+    @GetMapping(value = "/api/v1/upload/progress")
+    public ResponseEntity getFileUploadProgress(final HttpServletRequest request)
+    {
+        final String response = "No data";
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value = "/api/v1/upload/duration")
+    public ModelAndView getFileUploadDuration()
     {
         return new ModelAndView("upload");
     }
